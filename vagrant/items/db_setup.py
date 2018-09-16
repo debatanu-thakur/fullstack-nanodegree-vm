9f7cbca854c1754c3b3b5063521f1edd305dab0b
debatanu-thakur <debatanu@gmail.com>
2018-09-17 00:36:52 +0200

    Fix pep8 linting and add user password hashs

--- vagrant/items/db_setup.py
+++ vagrant/items/db_setup.py
@@ -6,29 +6,44 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.orderinglist import ordering_list
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
 
 
 Base = declarative_base()
+
 
 class User(Base):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, index=True)
+    password_hash = Column(String(64))
     picture = Column(String(250))
+    user_type = Column(Integer, nullable=False, default=2)
+    # user_type 1 - admin, 2 - normal user
+
+    def has_password(self, password):
+        self.password_hash = pwd_context.hash(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
 
 class Category(Base):
-    def __init__(self, items = []):
+    def __init__(self, items=[]):
         self._items = items
 
     __tablename__ = "catalog_category"
-    name = Column ( String(80), nullable = False )
-    id = Column( Integer, primary_key = True )
-    image = Column( String(255), nullable = True )
-    created_at = Column( DateTime, nullable=True, default=datetime.datetime.now )
-    last_updated = Column( DateTime, nullable=True, default=datetime.datetime.now, onupdate=datetime.datetime.now )
-    items=relationship("Item", order_by="Item.id", collection_class=ordering_list('id'))
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    image = Column(String(255), nullable=True)
+    created_at = Column(DateTime, nullable=True, default=datetime.datetime.now)
+    last_updated = Column(DateTime, nullable=True,
+                          default=datetime.datetime.now,
+                          onupdate=datetime.datetime.now)
+    items = relationship("Item", order_by="Item.id",
+                         collection_class=ordering_list('id'))
 
     @property
     def serialize(self):
@@ -41,17 +56,20 @@ class Category(Base):
             'items': [item.serialize for item in self.items]
         }
 
+
 class Item(Base):
     __tablename__ = "catalog_items"
-    
-    id = Column( Integer, primary_key = True )
-    title = Column( String(80), nullable = False )
-    description = Column( String(255), nullable = False )
-    image = Column( String(255), nullable = True )
-    is_deleted = Column( Boolean, nullable=False, default=False )
-    created_at = Column( DateTime, nullable=True, default=datetime.datetime.now )
-    last_updated = Column( DateTime, nullable=True, default=datetime.datetime.now, onupdate=datetime.datetime.now )
-    category_id = Column( Integer, ForeignKey("catalog_category.id") )
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(80), nullable=False)
+    description = Column(String(255), nullable=False)
+    image = Column(String(255), nullable=True)
+    is_deleted = Column(Boolean, nullable=False, default=False)
+    created_at = Column(DateTime, nullable=True, default=datetime.datetime.now)
+    last_updated = Column(DateTime, nullable=True,
+                          default=datetime.datetime.now,
+                          onupdate=datetime.datetime.now)
+    category_id = Column(Integer, ForeignKey("catalog_category.id"))
     category = relationship(Category)
 
     @property
@@ -67,7 +85,6 @@ class Item(Base):
             'categoryId': self.category_id
         }
 
-
-##### connect to db engine ######
+# Connect db engine
 engine = create_engine("sqlite:///catalog.db")
 Base.metadata.create_all(engine)
--- vagrant/items/main.py
+++ vagrant/items/main.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-app = Flask(__name__)
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from db_setup import Base, Category, Item, User
 
+app = Flask(__name__)
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
 
 DBSession = scoped_session(sessionmaker(bind=engine))
-
 
 
 @app.route('/')
@@ -18,7 +16,7 @@ def DefaultLanding():
     session = DBSession()
     categories = session.query(Category)
     for cat in categories:
-        items = session.query(Item).filter_by(category_id = cat.id)
+        items = session.query(Item).filter_by(category_id=cat.id)
         cat.items = items
     output = render_template('home.html', categories=categories)
     remove_session()
@@ -29,7 +27,8 @@ def DefaultLanding():
 def catalog(category_name):
     session = DBSession()
     category = session.query(Category).filter_by(name=category_name).one()
-    items = session.query(Item).filter_by(category_id = category.id, is_deleted=False)
+    items = session.query(Item).filter_by(category_id=category.id,
+                                          is_deleted=False)
     output = render_template('category.html', category=category, items=items)
     remove_session()
     return output
@@ -42,7 +41,8 @@ def addItem(category_name):
         image = ''
         session = DBSession()
         category = session.query(Category).filter_by(name=category_name).one()
-        item = Item(title=title, description=description, image=image, category_id=category.id)
+        item = Item(title=title, description=description, image=image, 
+                    category_id=category.id)
         session.add(item)
         session.commit()
         remove_session()
@@ -50,16 +50,19 @@ def addItem(category_name):
     else:
         session = DBSession()
         category = session.query(Category).filter_by(name=category_name).one()
-        output = render_template('add.html', category=category )
+        output = render_template('add.html', category=category)
         remove_session()
     return output
-# Task 2: Create route for editMenuItem function here
 
-@app.route('/catalog/<string:category_name>/<int:item_id>/edit/', methods=['GET', 'POST'])
+
+# Task 2: Create route for editMenuItem function here
+@app.route('/catalog/<string:category_name>/<int:item_id>/edit/',
+           methods=['GET', 'POST'])
 def editItem(category_name, item_id):
     session = DBSession()
     category = session.query(Category).filter_by(name=category_name).one()
-    item = session.query(Item).filter_by(id=item_id, category_id=category.id).one()
+    item = session.query(Item).filter_by(id=item_id,
+                                         category_id=category.id).one()
     if request.method == 'POST':
         item.title = request.form['title']
         item.description = request.form['description']
@@ -68,19 +71,20 @@ def editItem(category_name, item_id):
         remove_session()
         return redirect(url_for('catalog', category_name=category_name))
     else:
-        output = render_template('edit.html', category=category, item=item )
+        output = render_template('edit.html', category=category, item=item)
         remove_session()
     return output
 
-# Task 3: Create a route for deleteMenuItem function here
 
-@app.route('/catalog/<string:category_name>/<int:item_id>/delete/', methods=['GET', 'POST'])
+# Task 3: Create a route for deleteMenuItem function here
+@app.route('/catalog/<string:category_name>/<int:item_id>/delete/',
+           methods=['GET', 'POST'])
 def deleteItem(category_name, item_id):
     session = DBSession()
     category = session.query(Category).filter_by(name=category_name).one()
     item = session.query(Item).filter_by(id=item_id).one()
     if request.method == 'POST':
-        item.is_deleted=True
+        item.is_deleted = True
         session.add(item)
         session.commit()
         remove_session()
@@ -96,7 +100,7 @@ def JSONDefault():
     session = DBSession()
     categories = session.query(Category)
     for cat in categories:
-        items = session.query(Item).filter_by(category_id = cat.id)
+        items = session.query(Item).filter_by(category_id=cat.id)
     remove_session()
     output = jsonify(Categories=[cat.serialize for cat in categories])
     return output
@@ -107,7 +111,7 @@ def JSONDefaultAll():
     categories = session.query(Category)
     all_data = []
     for cat in categories:
-        items=session.query(Item).filter_by(category_id = cat.id)
+        items = session.query(Item).filter_by(category_id=cat.id)
         cat.items.extend(items)
     remove_session()
     output = jsonify(Categories=[cat.serialize for cat in categories])
@@ -133,6 +137,6 @@ def JSONItem(category_id, item_id):
 def remove_session():
     DBSession.remove()
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.debug = True
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host='0.0.0.0', port=8000)
